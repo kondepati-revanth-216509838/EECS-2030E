d3dfa8969e6d4ad2facb7149ea0ac0d8a9ed6386
Revanth Kondepati <revanth@my.yorku.ca>
2019-09-19 13:55:41 -0400

    Lab2 Upload
    Given Homework

---
+++ EECS2030_Lab2/src/eecs2030/lab2/Nickel.java
@@ -0,0 +1,13 @@
+package eecs2030.lab2;
+
+public class Nickel implements Comparable<Nickel> {
+
+	private int year;
+
+	/**
+	 * The monetary value of a nickel in cents.
+	 */
+	public final int CENTS = 5;
+
+
+}
